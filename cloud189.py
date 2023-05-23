aea084dd067b2be6eb2ebb34d28ec6f346f16e59
shwu1984 <shwu1984@163.com>
2023-05-23 09:38:25 +0800

    cloud189.py

--- cloud189.py
+++ cloud189.py
@@ -3,8 +3,8 @@ from urllib import parse
 
 s = requests.Session()
 
-username = "user"
-password = "pwd"
+username = "15605297035"
+password = "Sshwu@1984*"
 
 if(username == "" or password == ""):
     username = input("账号：")
